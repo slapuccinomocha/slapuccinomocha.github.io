7ea4efa7b387f6d87d02f591e8607f071cdcf9f0
slapuccinomocha <101086170+slapuccinomocha@users.noreply.github.com>
2025-03-25 04:19:32 +1100

    Responsiveness
    Added nav menu for smaller screensizes,
    animations for text
    link to membership form
    link to scrapbook

--- javascript/main.js
+++ javascript/main.js
@@ -12,34 +12,49 @@ uniPara.style.opacity = 0;
 scrapbookLink.style.opacity = 0;
 joinButton.style.opacity = 0;
 
+function sidebar_toggle() {
+  var x = document.getElementById("sidebar");
+  var y = document.getElementById("sidebar-icon");
+  var h = document.getElementsByClassName("body");
 
+  if (x.style.width === "100%") {
+    x.style.width = "0%";
+    y.src = "images/sidebar-icon.png";
+  } else {
+    x.style.width = "100%";
+    y.src = "images/exit-icon.png";
+  }
+}
 
-document.addEventListener('scroll', () => {
+document.addEventListener("scroll", () => {
   if (window.scrollY > 0) {
-    readMore.style.opacity = 10/window.scrollY*rate;
+    readMore.style.opacity = (10 / window.scrollY) * rate;
     // console.log(fabsocTitle.style.opacity);
-
   } else {
     readMore.style.opacity = 1;
   }
-
 });
 
 let callback = (entries, observer) => {
-  entries.forEach(entry => {
+  entries.forEach((entry) => {
     if (entry.isIntersecting) {
       fabsocTitle.style.opacity = 0;
       uniPara.style.opacity = 0;
       scrapbookLink.style.opacity = 0;
-      setTimeout(function(){joinButton.style.opacity = 0;}, 0000);
+      setTimeout(function () {
+        joinButton.style.opacity = 0;
+      }, 0000);
       console.log(fabsocTitle.style.opacity);
     } else {
       fabsocTitle.style.opacity = 1;
       uniPara.style.opacity = 1;
-      setTimeout(function(){scrapbookLink.style.opacity = 1;}, 1000);
-      setTimeout(function(){joinButton.style.opacity = 1;}, 2000);
+      setTimeout(function () {
+        scrapbookLink.style.opacity = 1;
+      }, 1000);
+      setTimeout(function () {
+        joinButton.style.opacity = 1;
+      }, 2000);
       console.log(fabsocTitle.style.opacity);
-
     }
   });
 };
