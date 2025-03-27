c8cf824ad66e4f7f0b111a0846bdffcea1f446fe
slapuccinomocha <101086170+slapuccinomocha@users.noreply.github.com>
2025-03-28 01:24:30 +1100

    Images and Anims
    Added images
    Updated team.html with some animations

--- javascript/team.js
+++ javascript/team.js
@@ -1,5 +1,10 @@
 // Grab elements from html doco
-const teamWhiteLine = document.getElementById("teamWhiteLine");
+const aryanIMG = document.getElementById("aryan");
+const ashnaIMG = document.getElementById("ashna");
+const aarnavIMG = document.getElementById("aarnav");
+const teamIMG = document.getElementById("4execs");
+const teamHead = document.getElementById("teamTextHead");
+const teamBody = document.getElementById("teamTextBody");
 
 // side menu toggle
 function sidebar_toggle() {
@@ -16,16 +21,36 @@ function sidebar_toggle() {
   }
 }
 
-// check for scroll event
+// scroll for execs show
 document.addEventListener("scroll", () => {
-  var whitelinegrowth = ((2 * window.scrollY) / 1150) * 100 - 200;
-  if (window.scrollY > 0 && whitelinegrowth < 0) {
-    teamWhiteLine.style.left = whitelinegrowth.toString() + "vw";
-    console.log(window.scrollY);
-    console.log(whitelinegrowth);
-  } else if (window.scrollY > 0 && whitelinegrowth > 0) {
-    teamWhiteLine.style.left = (-whitelinegrowth).toString() + "vw";
-    console.log(window.scrollY);
-    console.log(whitelinegrowth);
+  console.log(window.scrollY);
+  if (window.scrollY > 800 && window.scrollY < 1100) {
+    teamHead.textContent = "exec team:";
+    teamIMG.src = "./images/ourteam/execsPNG1.png";
+    teamBody.innerHTML =
+      "president | VPI: Arsh <br /> Treasury: Kavya <br />VPE: Aivy <br /> VPC: Indira <br /> welfare: Alex";
+  } else if (window.scrollY >= 1100 && window.scrollY < 1300) {
+    teamIMG.src = "./images/ourteam/execsPNG3.png";
+  } else if (window.scrollY >= 1300 && window.scrollY < 1500) {
+    teamIMG.src = "./images/ourteam/execsPNG4.png";
+  } else if (window.scrollY >= 1500 && window.scrollY < 1700) {
+    teamHead.textContent = "exec team:";
+    teamIMG.src = "./images/ourteam/execsPNG2.png";
+    teamBody.innerHTML =
+      "president | VPI: Arsh <br /> Treasury: Kavya <br />VPE: Aivy <br /> VPC: Indira <br /> welfare: Alex";
+  } else if (window.scrollY >= 1700 && window.scrollY < 1900) {
+    teamIMG.src = "./images/ourteam/aryan.png";
+    teamHead.textContent = "digitals team:";
+    teamBody.innerHTML = "Aryan <br> Aarnav <br> Ashna";
+  } else if (window.scrollY >= 1900 && window.scrollY < 2100) {
+    teamIMG.src = "./images/ourteam/aarnav.png";
+  } else if (window.scrollY >= 2100 && window.scrollY < 2300) {
+    teamIMG.src = "./images/ourteam/ashna.png";
+  } else if (window.scrollY >= 1900 && window.scrollY < 2100) {
+    teamIMG.src = "./images/ourteam/aryan2.jpeg";
+  } else if (window.scrollY >= 1900 && window.scrollY < 2100) {
+    teamIMG.src = "./images/ourteam/aryan2.jpeg";
+  } else if (window.scrollY >= 1900 && window.scrollY < 2100) {
+    teamIMG.src = "./images/ourteam/aryan2.jpeg";
   }
 });
