0b77af468b620cbb18627c77a42f3c1b82f4e8e7
slapuccinomocha <101086170+slapuccinomocha@users.noreply.github.com>
2025-04-09 11:43:27 +1000

    contact page added
    contact page added
    animation added for contact page

---
+++ javascript/contact.js
@@ -0,0 +1,40 @@
+const scrollers = document.querySelectorAll(".contactScroller");
+
+// side menu toggle
+function sidebar_toggle() {
+  var x = document.getElementById("sidebar");
+  var y = document.getElementById("sidebar-icon");
+  var h = document.getElementsByClassName("body");
+
+  if (x.style.width === "100%") {
+    x.style.width = "0%";
+    y.src = "images/sidebar-icon.png";
+  } else {
+    x.style.width = "100%";
+    y.src = "images/exit-icon.png";
+  }
+}
+
+addAnimation();
+
+function addAnimation() {
+  console.log(scrollers);
+  scrollers.forEach((scroller) => {
+    const scrollerInner = scroller.querySelector(".contactInnerScroll");
+    const scrollerCont = Array.from(scrollerInner.children);
+
+    scrollerCont.forEach((item) => {
+      const dupItem = item.cloneNode(true);
+      dupItem.setAttribute("aria-hidden", true);
+      scrollerInner.appendChild(dupItem);
+    });
+  });
+}
+
+document.addEventListener("scroll", () => {
+  if (window.scrollY > 0) {
+    readMore.style.opacity = (10 / window.scrollY) * rate;
+  } else {
+    readMore.style.opacity = 1;
+  }
+});
