51d01bec94117e13ca682fc62155db0b1f8c016c
slapuccinomocha <101086170+slapuccinomocha@users.noreply.github.com>
2025-04-15 01:44:51 +1000

    blog page added
    events card added
    all team images added
    blog menu added under ss25

--- javascript/events.js
+++ javascript/events.js
@@ -31,3 +31,54 @@ $("eventsCard").each(function () {
     .setTween(tween)
     .addTo(controller);
 });
+
+// function for generating pop-up window with relevant information.
+function generate_popup(eventID) {
+  // retrieve popup window and body
+  const mainbody = document.getElementById("mainbody");
+  const popUpWindow = document.getElementById("eventsPopUpFader");
+
+  // retrieve all cards from button that was pressed
+  const eventCard = document.querySelector(eventID);
+
+  // once known which event button was triggered retrieve info related to that event
+  const eventIMG = eventCard.querySelector("img");
+  const eventName = eventCard.querySelector("h2");
+  const eventLoc = eventCard.querySelectorAll("h5")[0];
+  const eventTime = eventCard.querySelectorAll("h5")[1];
+  const eventPrice = eventCard.querySelectorAll("h5")[2];
+  const eventDesc = eventCard.querySelector("p");
+
+  // name, location, price, event description from popupwindow
+  const popupIMG = popUpWindow.querySelector("img");
+  const popupName = popUpWindow.querySelector("h2");
+  const popupLoc = popUpWindow.querySelectorAll("h5")[0];
+  const popupTime = popUpWindow.querySelectorAll("h5")[1];
+  const popupPrice = popUpWindow.querySelectorAll("h5")[2];
+  const popupDesc = popUpWindow.querySelector("p");
+  console.log(popupDesc);
+
+  // allocate each piece of information to relevant field in popup
+  popupIMG.src = eventIMG.src;
+  popupName.innerText = eventName.innerText;
+  popupLoc.innerText = eventLoc.innerText;
+  popupTime.innerText = eventTime.innerText;
+  popupPrice.innerText = eventPrice.innerText;
+  popupDesc.innerText = eventDesc.innerText;
+
+  // prevent scrolling by overflowing hidden on body
+  mainbody.style.overflow = "hidden";
+
+  // display popup window
+  popUpWindow.style.display = "flex";
+}
+
+function close_popup() {
+  // retrieve exit button from popup
+  const popUpWindow = document.getElementById("eventsPopUpFader");
+  const mainbody = document.getElementById("mainbody");
+
+  // hide popUpWindow
+  mainbody.style.overflow = "initial";
+  popUpWindow.style.display = "none";
+}
